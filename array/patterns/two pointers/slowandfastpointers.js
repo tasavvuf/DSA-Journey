d992e241ba360e92ed998fce796c6ce3aa43a351
Tasavvuf Gori <tasavvufg@gmail.com>
2026-04-02 23:37:31 +0530

    another problem to be done

--- array/patterns/two pointers/slowandfastpointers.js
+++ array/patterns/two pointers/slowandfastpointers.js
@@ -1,19 +1,15 @@
-let nums = [1,1,1,2,2,2,2,2,3,3]
+nums = [0,1,0,3,12]
+val = 0
 let i = 0;
-console.log("rn i is ",i)
-for (let j = 1; j < nums.length; j++) {
-    console.log("j",j)
-    if (nums[j] !== nums[i]) {
-        console.log("conditon match at index",i,j,"and values",nums[i],nums[j]);
 
+for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+        nums[i] = nums[j];
         i++;
-        console.log("now i is" ,i);
-        console.log("before",nums.length)
-        nums[i] = nums[j];//  here is something i dont understand how they manuplate the array 
-        console.log("new array",nums)
-                console.log("after",nums.length)
-
     }
 }
-
-console.log("ans",i + 1) 
+while(i<nums.length){
+    nums[i]=0
+    i++
+}
+console.log(nums)
