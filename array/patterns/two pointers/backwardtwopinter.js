45d267b1e3cf639e79625b99cbfd387d8dc1b763
Tasavvuf Gori <tasavvufg@gmail.com>
2026-04-06 11:43:38 +0530

    learned a new pattern

---
+++ array/patterns/two pointers/backwardtwopinter.js
@@ -0,0 +1,21 @@
+let nums1 = [1,2,3,0,0,0]
+let m = 3
+let nums2 = [2,5,6]
+let n = 3
+let i = m-1
+let j = n-1
+let k = m+n -1
+while(j>=0){
+    if(i>=0 && nums1[i]>nums2[j]){
+        nums1[k] = nums1[i]
+        i--
+        console.log(nums1)
+
+    }
+    else{
+        nums1[k]=nums2[j]
+        j--
+        console.log(nums1)
+    }
+    k--
+}
