048afab546560252d3aafa3643b9ee5886ed4607
Tasavvuf Gori <tasavvufg@gmail.com>
2026-04-06 12:26:55 +0530

    solved using the two pointer ans get the O(n)

---
+++ array/MergeOverlappingSubintervals(optimal).js
@@ -0,0 +1,14 @@
+let a = [[1,4],[0,2],[3,5]]
+let merged = []
+a.sort((a,b)=>a[0]-b[0])
+console.log(a)
+for (const i of a) {
+    if(merged==0 || merged[merged.length-1][1]<i[0]){
+        merged.push(i)
+    }
+    else {
+        merged[merged.length-1][1] = Math.max(merged[merged.length-1][1] ,i[1] )
+    }
+    
+}
+console.log(merged)
