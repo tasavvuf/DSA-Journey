f3a2c6ebddd28b80bf08aa04ac2d3a7e02790a2d
Tasavvuf Gori <tasavvufg@gmail.com>
2026-04-11 10:14:07 +0530

    resolved and ssues and do dry run with the console log to get exact idea

--- array/patterns/two pointers/Longest substring without repeating characters.js
+++ array/patterns/two pointers/Longest substring without repeating characters.js
@@ -1,15 +1,22 @@
-let str = "abcabcbb"
-let set = new Set(str)
-console.log(set)
+let s = "abcabcbb"
+let set = new Set()
 let l = 0;
 let maxLen = 0;
-
+console.log("run start")
 for (let r = 0; r < s.length; r++) {
+     console.log("l :",s[l], `index ${l}   `,"r:",s[r],`index ${r}`)
     while (set.has(s[r])) {
         set.delete(s[l]);
         l++;
+        console.log("while loop run");
+        console.log("l :",s[l], `index ${l}   `,"r:",s[r],`index ${r}`)
+
     }
 
     set.add(s[r]);
+    console.log(set)
     maxLen = Math.max(maxLen, r - l + 1);
+    console.log("max len ",maxLen);
+    
 }
+console.log(maxLen)
